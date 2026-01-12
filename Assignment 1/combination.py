dff7112fbabaa263711f3638b087d37cfe243d5b
TrungNGM <samonix788@gmail.com>
2026-01-12 16:24:44 +0700

    Delete Assignment 1 directory

--- Assignment 1/combination.py
+++
@@ -1,7 +0,0 @@
-import random
-
-code_3_digit = [random.randint(0, 9) for _ in range(3)]
-code_4_digit = [random.randint(1, 6) for _ in range(4)]
-
-print("3-digit code:", code_3_digit)
-print("4-digit code:", code_4_digit)
--- Assignment 1/rectangle.py
+++
@@ -1,8 +0,0 @@
-length = float(input("enter the length of the rectangle:"))
-width = float(input("enter the width of the rectangle:"))
-
-perimeter = 2 * (length + width)
-area = length * width
-
-print("Perimeter:", perimeter)
-print("Area:", area)

1b6d305670760a17ce0a85296e4d2b72adfaf8b5
TrungNGM <samonix788@gmail.com>
2026-01-12 16:26:01 +0700

    Add files via upload

---
+++ Assignment 1/rectangle.py
@@ -0,0 +1,8 @@
+length = float(input("enter the length of the rectangle:"))
+width = float(input("enter the width of the rectangle:"))
+
+perimeter = 2 * (length + width)
+area = length * width
+
+print("Perimeter:", perimeter)
+print("Area:", area)

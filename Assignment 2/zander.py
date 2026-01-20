4741c863e84a887bff9ee3bf811b74bd119c404b
TrungNGM <samonix788@gmail.com>
2026-01-20 11:17:51 +0700

    Delete Assignment 2 directory

--- Assignment 2/zander.py
+++
@@ -1,7 +0,0 @@
-a = float(input("length of zander in centimeters:"))
-
-if a < 42:
-    print("the zander doesn't meet the limit, please release the fish back into the lake")
-    print("the size of the current Zander below the size limit is:", 42 - a)
-else:
-    print("the zander meets the size limit")

ad8c7cdf93f9b8447b2a1c020977c81079e0a823
TrungNGM <samonix788@gmail.com>
2026-03-07 20:02:28 +0700

    Delete Assignment 6 directory

--- Assignment 6/Seasons.py
+++
@@ -1,4 +0,0 @@
-seasons = ("Winter", "Spring", "Summer", "Autumn")
-months = int(input("Enter the month number (1-12): "))
-index = (months % 12) // 3
-print("The season for month", months, "is:", seasons[index])

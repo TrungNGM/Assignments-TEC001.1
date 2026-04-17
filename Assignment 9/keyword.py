67fa2230924a6b53e164f89ae759e7c82fa0aa3d
TrungNGM <samonix788@gmail.com>
2026-04-17 21:01:06 +0700

    Delete Assignment 9 directory

--- Assignment 9/keyword.py
+++
@@ -1,9 +0,0 @@
-def count_keywords(filename, keyword):
-    lines = []
-    
-    with open(filename, 'r') as file:
-        for i, line in enumerate(file, start=1):
-            if keyword in line:
-                lines.append(i)
-    return lines
-print(count_keywords("input.txt", "Python"))
--- Assignment 9/uppercase.py
+++
@@ -1,8 +0,0 @@
-def convert_uppercase(filename):
-    with open (filename, 'r') as infile:
-        content = infile.read()
-    content = content.upper()
-    with open ('output.txt', 'w') as outfile:
-        outfile.write(content)
-
-convert_uppercase("input.txt")

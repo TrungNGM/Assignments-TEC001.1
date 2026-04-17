f5b1fcff49f4a3b32de8b8c6408f11fafced746c
TrungNGM <samonix788@gmail.com>
2026-04-17 21:05:44 +0700

    Delete Assignment 10 directory

--- Assignment 10/ChuckNorris.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-request = "https://api.chucknorris.io/jokes/random"
-response = requests.get(request).json()
-# print(json.dumps(response))
-print(response["value"])
--- Assignment 10/front.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-request = "http://127.0.0.1:5000/airport/EFHK"
-response = requests.get(request).json()
-print(response)
